da832cc79be6b3f14aca48125f1394e4cc2f5b3d
tenSun <a0985092384@gmail.com>
2026-03-22 18:12:07 +0800

    feat: update GetApp component with button mapping and enhance styling, refactor image to use Next.js Image component

--- components/sections/GetApp.tsx
+++ components/sections/GetApp.tsx
@@ -1,37 +1,48 @@
 import { Button, BUTTON_VARIANTS } from '../ui'
+import Image from 'next/image'
+
+const GET_APP_BUTTONS = [
+  {
+    title: 'App Store',
+    icon: '/apple.svg',
+    variant: BUTTON_VARIANTS.WHITE,
+  },
+  {
+    title: 'Play Store',
+    icon: '/android.svg',
+    variant: BUTTON_VARIANTS.DARK_GREEN_OUTLINE,
+  },
+] as const
 
 const GetApp = () => {
   return (
-    <section className="flexCenter w-full flex-col pb-[100px]">
-      <div className="get-app">
-        <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
+    <section className="flexCenter w-full flex-col bg-red-400 pb-[100px]">
+      <div className="get-app bg-blue-300">
+        <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12 bg-yellow-200">
           <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get for free now!</h2>
           <p className="regular-16 text-gray-10">Available on iOS and Android</p>
+          {/* Button group */}
           <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
-            <Button
-              type="button"
-              title="App Store"
-              icon="/apple.svg"
-              variant={BUTTON_VARIANTS.WHITE}
-              full
-            />
-            <Button
-              type="button"
-              title="Play Store"
-              icon="/android.svg"
-              variant={BUTTON_VARIANTS.DARK_GREEN_OUTLINE}
-              full
-            />
+            {GET_APP_BUTTONS.map((btn) => (
+              <Button
+                key={btn.title}
+                type="button"
+                title={btn.title}
+                icon={btn.icon}
+                variant={btn.variant}
+                full
+              />
+            ))}
           </div>
         </div>
-
         <div className="flex flex-1 items-center justify-end">
-          <img
+          <Image
             src="/phones.png"
             alt="phones"
             width={550}
             height={870}
-            style={{ width: 'auto', height: 'auto' }}
+            priority // If this image is on the first screen (Above the fold), adding this can improve LCP performance.
+            className="h-auto w-full max-w-[550px] object-contain"
           />
         </div>
       </div>
